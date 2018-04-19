37d63019ee1e2d61dd4dc8ed939cbaec58d8cf76
agazda1 <31972328+agazda1@users.noreply.github.com>
2018-04-18 23:50:33 -0700

    just needs graphs!

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def main():
     results.make_tables(cur, conn)
     results.create_results(cur, conn)
 
+    print("Creating database...")
+
     # Populate the Candidate table
     candidates.populate_candidate_table(cur, conn)
 
@@ -29,7 +31,7 @@ def main():
 
     cur.execute("""
     SELECT name FROM State
-    ORDER BY election_date
+    ORDER BY election_date ASC
     LIMIT 25;
     """)
     states_list = cur.fetchall()
@@ -46,38 +48,104 @@ def main():
         for state in states_list:
             results.insert_result(cur, conn, state[0], hopeful[0], randint(20,300))
 
+
+    print("Half of the election results are in!")
     choice = ""
-    """
+
     while (choice != 7):
         print("-------------------------")
         print("Please select an option:")
-        # insert_result(curr, conn, "CA", "Kanye West", 500)
         print("1. Add new result")
-        # delete_result_row (curr, conn, "CA", "Oprah Winfrey")
         print("2. Delete old result")
-        # print(display_all_candidates(curr))
-
-        # print(display_candidates_and_ids(curr))
-        # print(display_all_male_competitors(curr))
-        # print(display_all_female_competitors(curr))
-        # print(display_candidates_by_party(curr, "Democrat"))
-        # print(display_candidates_by_party(curr, "Republican"))
         print("3. Display candidates by format")
-        # print(display_all_events(curr))
         print("4. Display all events")
-        # print(display_top_competitors(curr, "CA"))
-        # print(display_top_competitors(curr, "VE")) # empty list (no data)
         print("5. Display top competitors by state")
-        # print(look_up_id (curr, "Oprah Winfrey", 64))
-        # print(look_up_info_by_id (curr, 1))
-        # print(look_up_total_votes_by_name(curr, "Oprah Winfrey"))
-        # print(look_up_total_votes_by_id (curr, 1))
-        # print(look_up_delegate_tally (curr, 1))
         print("6. Search")
         print("7. I'm done!")
 
         choice = int(input())
-    """
+        if choice == 7:
+            continue
+
+        elif choice == 1:
+            state = input("For which state? ")
+            cand = input("For which candidate? ")
+            votes = int(input("How many votes? "))
+            results.insert_result(cur, conn, state, cand, votes)
+
+        elif choice == 2:
+            state = input("For which state? ")
+            cand = input("For which candidate? ")
+            results.delete_result_row (cur, conn, state, cand)
+
+        elif choice == 3:
+            next = ""
+            while next != 6:
+                print("------------")
+                print("Select what number you would like to display:")
+                print("1. All candidates alphabetically")
+                print("2. All Candidates and their IDs")
+                print("3. All male competitors")
+                print("4. All female competitors")
+                print("5. All candidates by party")
+                print("6. Nevermind!")
+
+                next = int(input())
+
+                if next == 6:
+                    continue
+                elif next == 1:
+                    print(results.display_all_candidates(cur))
+                elif next == 2:
+                    print(results.display_candidates_and_ids(cur))
+                elif next == 3:
+                    print(results.display_all_male_competitors(cur))
+                elif next == 4:
+                    print(results.display_all_female_competitors(cur))
+                elif next == 5:
+                    party = input("Display which party? ")
+                    print(results.display_candidates_by_party(cur, party))
+
+        elif choice == 4:
+            print(results.display_all_events(cur))
+
+        elif choice == 5:
+            state = input("Display top competitors for which state?")
+            print(results.display_top_competitors(cur, state))
+
+        elif choice == 6:
+            my_choice = ""
+            while my_choice != 6:
+                print("------------")
+                print("Select which number you would like to search for:")
+                print("1. Candidate ID by name and age")
+                print("2. Candidate info by ID")
+                print("3. Total Votes by candidate name")
+                print("4. Total Votes by ID")
+                print("5. Total Delegates by ID")
+                print("6. Nevermind!")
+
+                my_choice = int(input())
+
+                if my_choice == 6:
+                    continue
+                elif my_choice == 1:
+                    cand = input("Which candidate? ")
+                    age = int(input("What age? "))
+                    print(results.look_up_id (cur, cand, age))
+                elif my_choice == 2:
+                    this_id = int(input("Which ID? "))
+                    print(results.look_up_info_by_id (cur, this_id))
+                elif my_choice == 3:
+                    cand = input("Which candidate? ")
+                    print(results.look_up_total_votes_by_name(cur, cand))
+                elif my_choice == 4:
+                    this_id = int(input("Which ID? "))
+                    print(results.look_up_total_votes_by_id (cur, this_id))
+                elif my_choice == 5:
+                    this_id = int(input("Which ID? "))
+                    print(results.look_up_delegate_tally (cur, this_id))
+
 
 
 
